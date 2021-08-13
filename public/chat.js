10de320bf061a7287e8de3397f86697d29f6240f
Rafael Pufal <rafael.pufal23@gmail.com>
2021-08-13 19:29:56 -0300

    Primeiro commit

---
+++ public/chat.js
@@ -0,0 +1,28 @@
+const socket = window.io();
+
+const sendButton = document.querySelector('#send-button');
+const inputMessage = document.querySelector('#messageInput');
+const usersUl = document.querySelector('.users');
+// const nickname = document.querySelector('.users');
+sendButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  socket.emit('clientMessage', { chatMessage: inputMessage.value, 
+    nickname: usersUl.firstChild.innerText });
+  inputMessage.value = '';
+  return false;
+});
+
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.innerText = message;
+  messagesUl.prepend(li);
+};
+const createUser = (id) => {
+  const li = document.createElement('li');
+  li.innerText = id;
+  usersUl.appendChild(li);
+};
+
+socket.on('serverMessage', (message) => createMessage(message));
+socket.on('newUser', ({ id }) => createUser(id));
--- server.js
+++ server.js
@@ -1,1 +1,41 @@
-// Faça seu código aqui
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const socketIoServer = require('http').createServer(app);
+
+const io = require('socket.io')(socketIoServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT'],
+  },
+});
+
+io.on('connection', (socket) => {
+  io.emit('newUser', { id: socket.id });
+  socket.on('clientMessage', ({ chatMessage, nickname }) => {
+    console.log(`Mensagem ${chatMessage} do ${nickname}`);
+    const date = new Date();
+    const finalDate = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`; 
+    const finalTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const finalMessage = `${finalTime} ${finalDate} - ${nickname} ${chatMessage}`;
+    io.emit('serverMessage', finalMessage);
+  });
+});
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', (_req, res) => {
+  res.status(200).render('chat');
+});
+
+socketIoServer.listen(PORT, () => {
+  console.log(`Socket.io listening on port ${PORT}`);
+});
