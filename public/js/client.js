c73d1d13b556f48a892e8e2121c354ec29d250da
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-13 15:49:36 -0300

    correçao capturas dos elementos

--- public/js/client.js
+++ public/js/client.js
@@ -5,23 +5,24 @@ const saveName = getEl('save_name');
 const clientsArea = getEl('text_area_clients');
 const inputMessages = getEl('input_messages');
 const sendMessage = getEl('send_message');
-const messagesArea = getEl('text_area_clients');
+const messagesArea = getEl('text_area_messages');
 
 inputName.addEventListener('change', () => {
-  console.log('to funfando');
+  console.log('to funfando - inputname');
 });
 saveName.addEventListener('click', () => {
-  console.log('to funfando');
+  console.log('to funfando - savename');
 });
 clientsArea.addEventListener('change', () => {
-  console.log('to funfando');
+  console.log('to funfando- clientsaerea');
 });
 inputMessages.addEventListener('change', () => {
-  console.log('to funfando');
+  console.log('to funfando - inputmessage');
 });
-sendMessage.addEventListener('clcik', () => {
-  console.log('to funfando');
+
+sendMessage.addEventListener('click', () => {
+  console.log('to funfando - senmessage');
 });
 messagesArea.addEventListener('change', () => {
-  console.log('to funfando');
+  console.log('to funfando- messagearea');
 });
