5482cadb1af4a692485fc3bff45a30fd066d8dc0
johnealves <johne_alves@hotmail.com>
2021-08-13 18:24:34 -0300

    requirement 1

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 // Faça seu código aqui
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+// const { Server } = require('socket.io');
+const app = express();
+const socketServer = require('http').createServer(app);
+// const io = new Server(socketServer);
+
+const io = require('socket.io')(socketServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ['GET', 'POST'],
+  }
+});
+
+require('./socket/chat')(io);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.status(200).render('index');
+  // res.sendFile(__dirname + '/index.html')
+});
+
+const { PORT = 3000 } = process.env;
+
+socketServer.listen(PORT, () => console.log(`listen on port ${PORT}`));
---
+++ socket/chat.js
@@ -0,0 +1,21 @@
+var moment = require('moment'); // require
+moment().format(); 
+
+module.exports = (io) => {
+  io.on('connection', (socket) => {
+    console.log(`Usuário conectado: ${socket.id} `);
+
+    socket.on('set_nickname', (nickname) => {
+      socket.nickname = nickname;
+    })
+
+    socket.on('message', ({ chatMessage, nickname }) => {
+      const date = moment().format('DD-MM-YYYY h:mm:ss a');
+      io.emit('message', `${date} - ${nickname}: ${chatMessage}`)
+    })
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+}
