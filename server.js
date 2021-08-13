26f84fa5b7f19524358fe9f831bec0110fe6d152
Qu4k3r <neves0007@gmail.com>
2021-08-13 18:36:38 -0300

    Layout & servidor do chat prontos

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // Faça seu código aqui
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const socketIoServer = require('http').createServer(app);
+
+const io = require('socket.io')(socketIoServer);
+
+// io.on('connection', (socket) => {});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, 'public'));
+});
+
+socketIoServer.listen(3000, () => console.log('Listening on *:3000'));
