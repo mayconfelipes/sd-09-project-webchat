a1f6418d58dd9eb1ba5de13190b0d7433204d7fc
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-13 20:42:18 -0300

    estudando uma nova abordagem

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
 const express = require('express');
+const { join } = require('path');
 
 const app = express();
 const http = require('http').createServer(app);
 
-// const io = require('socket.io')(http, {
+const io = require('socket.io')(http);
+//   {
 //   cors: {
 //     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST'], 
+//     methods: ['GET', 'POST'],
 //   },
-// });
+// }
 
 const PORT = process.env.PORT || 3000;
 // require('./sockets/chat')(io);
 
-app.use(express.static(__dirname));
+app.use(express.static(join(__dirname, 'public')));
+app.set('views', join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+app.use('/', (req, res) => {
+  res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', (socket) => {
+  console.log(`socket conectado: ${socket.id}`);
+  
+  socket.on('sendMessage', (data) => {
+    messages.push(data);
+    socket.broadcast.emit('receivedMessage', data);
+  });
 });
 
 http.listen(PORT, () => {
