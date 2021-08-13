7b917b33f75c2ec8ad665ff7e5d454e61e168ce2
Felipe Cardoso <felipedclc@gmail.com>
2021-08-13 16:57:20 -0300

    Iniciando o projeto WebChat, VQV

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const cors = require('cors');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs'); // necessario para usar a engine(ejs)
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization'],
+  }),
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (_req, res) => res.send('Hello World!'));
+
+app.listen(PORT, () => console.log(`Express app listening on ${PORT}!`));
