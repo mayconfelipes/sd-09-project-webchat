179abe0f94931e79296c63acda2c2dd19b78d3cb
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-08-13 16:01:53 -0300

    Teste ping entre front e back

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 // Faça seu código aqui
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id} `);
+
+  socket.on('ping', () => {
+    console.log(`${socket.id} emitiu um ping!`);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
