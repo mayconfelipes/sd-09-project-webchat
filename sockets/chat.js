ee08dfcae8fd3c2388346c785873f239ac80a66a
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-13 19:34:55 -0300

    feat/requirement 2

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,5 +1,10 @@
+const crypto = require('crypto');
+
 module.exports = (io) => io.on('connection', (socket) => {
-  console.log(socket.id);
+  const firstNick = crypto.randomBytes(8).toString('hex');
+  socket.emit('myNickname', firstNick);
+  socket.broadcast.emit('nickname', { nickname: firstNick, id: socket.id });
+
   socket.on('message', ({ chatMessage, nickname }) => {
     const timestamps = new Date().toLocaleString('pt-br').replace(/\//g, '-');
     const message = `${timestamps} - ${nickname}: ${chatMessage}`;
@@ -7,6 +12,7 @@ module.exports = (io) => io.on('connection', (socket) => {
   });
 
   socket.on('nickname', (nickname) => {
-    io.emit('nickname', nickname);
+    socket.emit('myNickname', nickname);
+    socket.broadcast.emit('nickname', { nickname, id: socket.id });
   });
 });
--- view/js/chat.js
+++ view/js/chat.js
@@ -1,9 +1,13 @@
 const socket = window.io();
 
+// console.log((Math.random().toString(36).substring(2, 14) + Math.random().toString(36).substring(2, 7)).length);
+
 const formMessages = document.querySelector('#formMessages');
 const formNickname = document.querySelector('#formNickname');
 const inputMessage = document.querySelector('#messageInput');
 const inputNickName = document.querySelector('#nicknameInput');
+const usersUl = document.querySelector('#users');
+const myNickname = document.querySelector('#myNickname');
 
 const objMessage = {
   chatMessage: '',
@@ -28,15 +32,30 @@ const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
-const createListNickname = (nickname) => {
-  const usersUl = document.querySelector('#users');
-  const li = document.createElement('li');
-  li.innerText = nickname;
-  usersUl.appendChild(li);
+const updateNickname = (newNickname, id) => {
+  const li = document.querySelector(`#${id}`);
+  li.innerText = newNickname;
+};
+
+const createLiNickname = (nickname, id) => {
+  const li = document.querySelector(`#${id}`);
+  if (li) return updateNickname(nickname, id);
+
+  const newLi = document.createElement('li');
+  newLi.id = id;
+  newLi.innerText = nickname;
+  newLi.setAttribute('data-testid', 'online-user');
+  usersUl.appendChild(newLi);
+};
+
+const insertMyNickname = (nick) => {
+  myNickname.innerText = nick;
 };
 
 socket.on('message', (message) => createMessage(message));
-socket.on('nickname', (nickname) => createListNickname(nickname));
+socket.on('myNickname', (nick) => insertMyNickname(nick));
+socket.on('nickname', ({ nickname, id }) => createLiNickname(nickname, id));
