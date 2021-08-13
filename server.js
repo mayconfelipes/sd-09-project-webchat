1615c6e0674f5ee72812d28d40b7cde3022e41fe
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-13 15:24:26 -0300

    configuraçoes iniciais de server.js, e index.html

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
+});
+
+const PORT = process.env.PORT || 3000;
+// require('./sockets/chat')(io);
+
+// app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+http.listen(PORT, () => {
+  console.log('The Peoople Is On the Table!!!');
+});
