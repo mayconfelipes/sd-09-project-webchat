a965b4d24101a08ce827716789ed917731c936e8
Layo Kaminski <layokaminski@gmail.com>
2021-08-13 15:03:49 -0300

    Iniciando o projeto WebChat. VAMOS COM TUDO

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// Faça seu código aqui
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const http = require('http').createServer(app);
+
+const { PORT } = process.env;
+
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: 'http://localhost:3000', // url aceita pelo cors
+//     methods: ['GET', 'POST'], // Métodos aceitos pela url
+//   },
+// });
+
+// app.use(express.static(__dirname + '/public'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
