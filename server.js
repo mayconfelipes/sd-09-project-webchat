f832af2dbe674e8554f73edbba9765a5da2ff80a
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-08-13 16:11:32 -0300

    First commit

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+
+server.listen(3000, () => console.log('Conectado na porta 3000'));
