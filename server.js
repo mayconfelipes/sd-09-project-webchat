f4be1d5d19e280ecbd8345d911879c32cbf3219a
Lucas Silva <martssilvapro@gmail.com>
2021-08-13 16:35:55 -0300

    configura server e atualiza nome da view para views

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const app = require('express')();
 const http = require('http').createServer();
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST', 'PUT']
+  }
+})
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+io.on('connection', (socket) => {
+  console.log('Alguém entrou na sala');
+})
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/chat.ejs');
-})
+  res.status(200).render('chat')
+});
+
+http.listen(3000, () => {
+  console.log('Conectado na porta 3000');
+});
