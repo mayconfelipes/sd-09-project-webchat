8a9ccb1e887c62c7389520967fa4d11b712627c7
ggaldino95 <ggaldino95@gmail.com>
2021-08-14 18:12:47 -0300

    [Task 4] done

--- public/client.js
+++ public/client.js
@@ -1,9 +1,55 @@
 const socket = window.io(); // Chama o <script src="/socket.io/socket.io.js"></script>
+const DATA_TESTID = 'data-testid';
 
-const user = `User-${Math.random().toString().slice(2, 13)}`;
-const nick = document.querySelector('#user-nickname');
-nick.textContent = user;
-localStorage.setItem('nickname', JSON.stringify(user));
+socket.on('connect', () => { // Gera o nickname randomico e envia para o back-end
+  const user = `User-${Math.random().toString().slice(2, 13)}`;
+  localStorage.setItem('nickname', JSON.stringify(user));
+  const nick = document.querySelector('#user-nickname');
+  nick.textContent = user;
+  socket.emit('create user', user);
+});
+
+socket.on('append user', ({ id, nickname }) => { // Renderiza o usuario no front-end
+  const userWindow = document.querySelector('#users-list');
+  const onlineUser = document.createElement('li');
+  onlineUser.setAttribute(DATA_TESTID, 'online-user');
+  onlineUser.textContent = nickname;
+  onlineUser.id = id;
+  userWindow.appendChild(onlineUser);
+});
+
+socket.on('get user list', (userList) => { // Renderiza a lista de usuarios no front-end
+  const userWindow = document.querySelector('#users-list');
+  userList.forEach((user) => {
+    // if (user.id !== document.querySelector(`#${user.id}`)) {
+      const userOnList = document.createElement('li');
+      userOnList.setAttribute(DATA_TESTID, 'online-user');
+      userOnList.id = user.id;
+      userOnList.textContent = user.nickname;
+      userWindow.appendChild(userOnList);
+    // }
+  });
+});
+
+socket.on('message', (message) => { // Recebe a mensagem formatada do back-end e renderiza no front-end
+  const chatWindow = document.querySelector('#messages-list');
+  const msg = document.createElement('li');
+  msg.setAttribute(DATA_TESTID, 'message');
+  msg.textContent = message;
+  chatWindow.appendChild(msg);
+});
+
+socket.on('update nickname', ({ id, nickname }) => { // Recebe o nickname atualizado e faz alteracoes no front-end
+  const userOnList = document.querySelector(`#${id}`);
+  const userNickname = document.querySelector('#user-nickname');
+  userOnList.textContent = nickname;
+  userNickname.textContent = nickname;
+});
+
+socket.on('remove user', (userToBeRemoved) => { // Remove o usuario que se desconectou da lista no front-end
+  const userLi = document.querySelector(`#${userToBeRemoved.id}`);
+  userLi.remove();
+});
 
 // evento que captura a mensagem que o usuario digitou
 const messageForm = document.querySelector('#form');
@@ -29,28 +75,10 @@ updateNicknameButton.addEventListener('click', () => {
   const input = document.querySelector('#nickname-box');
 
   if (input.value) {
-    socket.emit('nickname', input.value);
+    socket.emit('update nickname', input.value); // o nickname no input e enviada ao back-end para atualizar o usuario
     localStorage.setItem('nickname', JSON.stringify(input.value));
     input.value = '';
   }
 
   return false;
 });
-
-const updateNickname = (nickname) => {
-  nick.textContent = nickname;
-};
-
-const sendMessage = (message) => {
-  const chatWindow = document.querySelector('#messages-list');
-  const msg = document.createElement('li');
-  msg.setAttribute('data-testid', 'message');
-  msg.textContent = message;
-  chatWindow.appendChild(msg);
-  // window.scrollTo(0, document.body.scrollHeight);
-};
-
-// recebe a mensagem formatada do back-end e renderiza no front-end
-socket.on('message', (message) => sendMessage(message));
-
-socket.on('nickname', (nickname) => updateNickname(nickname));
--- sockets/webchat.js
+++ sockets/webchat.js
@@ -1,24 +1,31 @@
 const { format } = require('date-fns');
 const messagesModel = require('../models/messages');
 
-const webChat = (io) => {
-  io.on('connection', (socket) => {
-    console.log(`Usuario ${socket.id} conectado.`);
-  
-    socket.on('nickname', (nickname) => {
-      io.emit('nickname', nickname);
-    });
-    
-    socket.on('message', ({ nickname, chatMessage }) => {
-      const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
-      messagesModel.updateHistory({ timestamp, nickname, message: chatMessage });
-      io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
-    });
-    
-    socket.on('disconnect', () => {
-      console.log(`Usuario ${socket.id} desconectou`);
-    });
+let usersList = [];
+const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
+
+const webChat = (io) => io.on('connection', (socket) => {
+  socket.on('create user', (nickname) => {
+    io.emit('append user', { id: socket.id, nickname });
+    usersList.push({ id: socket.id, nickname });
+    socket.emit('get user list', usersList.filter((user) => user.id !== socket.id));
   });
-};
+
+  socket.on('message', ({ nickname, chatMessage }) => {
+    messagesModel.updateHistory({ timestamp, nickname, message: chatMessage });
+    io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
+  });
+
+  socket.on('update nickname', (nickname) => {
+    usersList.find((user) => user.id === socket.id).nickname = nickname;
+    io.emit('update nickname', { id: socket.id, nickname });
+  });
+
+  socket.on('disconnect', () => {
+    const userToBeRemoved = usersList.find((user) => user.id === socket.id);
+    usersList = usersList.filter((user) => user.id !== socket.id);
+    io.emit('remove user', userToBeRemoved);
+  });
+});
 
 module.exports = webChat;
