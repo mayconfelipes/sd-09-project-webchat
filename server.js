b66454de5fec6274bb17417ab56c9b86cc6c31ce
BBrolesi <brunohsbrolesi@gmail.com>
2021-08-14 17:54:22 -0300

    complete requirement 2

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+
+const crypto = require('crypto');
 const path = require('path');
 const express = require('express');
 const { format } = require('date-fns');
@@ -6,6 +8,7 @@ const { format } = require('date-fns');
 const app = express();
 const http = require('http').createServer(app);
 
+const users = {};
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000',
@@ -17,19 +20,30 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log(`Usuário conectado, ID: ${socket.id}`);
+  socket.on('newUser', (callback) => {
+    const nickname = crypto.randomUUID().substring(0, 16);
+    users[socket.id] = nickname;
+    callback({ nickname });
+    io.emit('updateUsers', { users });
+  });
 
   socket.on('disconnect', () => {
-    console.log(`Usuário desconectado, ID: ${socket.id}`);
+    delete users[socket.id];
+    io.emit('updateUsers', { users });
   });
 
   socket.on('message', ({ chatMessage, nickname }) => {
     const date = format(new Date(), 'dd-MM-yyy hh:mm:ss a');
     io.emit('message', `${date} ${nickname} ${chatMessage}`);
   });
+
+  socket.on('updateNickname', ({ nickname }) => {
+    users[socket.id] = nickname;
+    io.emit('updateUsers', { users });
+  });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 http.listen(port, () => {
   console.log(`Servidor ouvindo na porta ${port}`);
