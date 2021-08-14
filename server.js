a279be24bdd2f1d65070945bd2aacc4edab3f17a
Qu4k3r <neves0007@gmail.com>
2021-08-13 22:46:54 -0300

    Transforma arquitetura em MVC | Alteracao no estilo do container das mensagens

--- server.js
+++ server.js
@@ -13,20 +13,25 @@ const io = require('socket.io')(socketIoServer);
 
 io.on('connection', (socket) => {
   socket.on('newUser', (userName) => {
+    if (!userName) return;
     io.emit('addNewUser', userName);
 
     socket.emit('userName', userName);
   });
 
   socket.on('message', ({ chatMessage, nickname }) => {
+    if (!chatMessage || !nickname) return;
     io.emit('message', `${currentTime} - ${nickname}: ${chatMessage}`);
   });
 });
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (_, res) => {
-  res.sendFile(path.join(__dirname, 'public'));
+  res.render('index');
 });
 
 socketIoServer.listen(3000, () => console.log('Listening on *:3000'));
