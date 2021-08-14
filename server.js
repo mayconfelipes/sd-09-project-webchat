4373b8171f5697d7f7565147d08e08396083b1b1
lcgoandete <lcgoandete@hotmail.com>
2021-08-13 23:01:09 -0300

    requisito 01

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-// Faça seu código aqui
+require('dotenv').config();
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const { Server } = require('socket.io');
+const ejs = require('ejs').renderFile;
+const moment = require('moment');
+
+const { PORT } = process.env;
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'src', 'views', 'home')));
+app.set('views', path.join(__dirname, 'src', 'views', 'home'));
+app.engine('html', ejs);
+app.set('view engine', 'html');
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    const dateTime = moment().format('DD-MM-YYYY HH:mm:ss');
+    const msg = ` ${dateTime} - ${data.nickname}:  ${data.chatMessage}`;
+    io.emit('message', msg);
+    console.log(msg);
+  });
+});
+
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
---
+++ src/views/home/chat.js
@@ -0,0 +1,3 @@
+// import  {socket} from '/socket.io/socket.io.js';
+
+//   const socket = io('http://localhost:3000');
