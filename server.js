05c2bd8ee94949aceacee406e791169ad24808c1
pedromchenrique <pedro.876.ph@gmail.com>
2021-08-14 14:08:37 -0300

    req 01

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-// Faça seu código aqui
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { format } = require('date-fns');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const timestamp = format(new Date(), 'dd-MM-yyyy HH:mm:ss');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/view/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado. ID: ${socket.id}`);
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${timestamp} ${nickname}: ${chatMessage}`);
+  });
+});
+
+http.listen(3000, () => console.log('Pai ta on na porta 3000'));
