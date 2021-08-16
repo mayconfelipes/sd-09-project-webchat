daa42167089cecc0475d12ea6d796282d1183258
Daniela Muller <danielamuller20@gmail.com>
2021-08-16 20:22:45 -0300

    req 4

--- sockets/chat.js
+++ sockets/chat.js
@@ -12,28 +12,51 @@ const dateFormat = ({ nickname, chatMessage }, socket) => {
   };
 };
 
+const arr = [];
+
+const onDisconnection = (socket, io, nickName) => {
+  socket.on('disconnect', () => {
+    const arrayIndex = arr.indexOf(nickName);
+    arr.splice(arrayIndex, 1);
+    io.emit('disconnection', nickName);
+  });
+};
+
+const allUsers = (socket, io) => {
+  socket.on('allUsers', () => {
+    io.emit('allUsers', arr);
+    console.log('all', arr);
+  });
+};
+
+const editName = (socket, io, nickName) => {
+  socket.on('editName', (newName) => {
+    io.emit('editName', ({ newName, nickName }));
+    const arrayIndex = arr.indexOf(nickName);
+    arr.splice(arrayIndex, 1, newName);
+  });
+};
 const chatIo = (io) => {
   io.on('connection', async (socket) => {
-    console.log(`Socket conectado com id: ${socket.id}`);
+    const nickName = randomstring.generate(16);
+    arr.push(nickName);
+    console.log('push', nickName);
+
     const messages = await chatContRoller.getAllMessages();
-
     socket.emit('previousMessage', messages);
-  
-    socket.emit('randomNickName', randomstring.generate(16));
 
-    socket.on('editNickName', async (data) => {
-      console.log(data);
-      // const updateUser = await chatContRoller.updateChatUser(dateFormat(data));
-      const chatMessages = await chatContRoller.postChatMessages(dateFormat(data));
-      io.emit('editNickName', JSON.stringify(chatMessages));
-    });
-  
+    socket.emit('randomNickName', JSON.stringify(nickName));
+
+    allUsers(socket, io);
+
+    editName(socket, io, nickName);
+
     socket.on('message', async (data) => {
       const chatMessages = await chatContRoller.postChatMessages(dateFormat(data, socket));
-      console.log(chatMessages);
-      console.log(socket.id);
       io.emit('message', JSON.stringify(chatMessages));
     });
+
+    onDisconnection(socket, io, nickName);
   });
 };
 
