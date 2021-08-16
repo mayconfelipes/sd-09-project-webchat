f3d794eb446e026d03db252dae384767de38eb79
Christopher <chris.tryber@gmail.com>
2021-08-16 13:17:57 -0300

    Requisito pronto

--- utils/getFullDate.js
+++ utils/getFullDate.js
@@ -6,7 +6,7 @@ function getFullDate() {
     const hour = String(data.getHours()).padStart(2, 0);
     const minute = String(data.getMinutes()).padStart(2, 0);
     const Seconds = String(data.getSeconds()).padStart(2, 0);
-    return `${day}-${month}-${fullYear} ${hour}-${minute}-${Seconds}`;
+    return `${day}-${month}-${fullYear} ${hour}:${minute}:${Seconds}`;
     // codigo baseado no link https://www.google.com/search?q=como+pegar+a+data+atual+em+javascript&oq=como+pegar+a+dta+&aqs=chrome.1.69i57j0i13l9.4871j0j7&sourceid=chrome&ie=UTF-8
 }
 
