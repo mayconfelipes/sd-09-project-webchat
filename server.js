2adaa289fd7889adf5ee99dc9da11d3f3c305be7
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-08-16 17:09:35 -0300

    bora que bora

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-// Faça seu código aqui
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = 3000; 
+
+const io = require('socket.io')(http, {
+    cors: {
+        origin: 'https://localhost:3000',
+        methods: ['GET', 'POST'],
+    },
+});
+
+app.use(express.static(`${__dirname}/web`));
+
+require('./sockets/webChat')(io);
+
+app.get('/', (_req, res) => {
+    res.sendFile(`${__dirname}/web/index.html`);
+});
+
+http.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
---
+++ web/js/chat.js
@@ -0,0 +1,86 @@
+const socket = window.io();
+
+window.onload = () => {
+  socket.emit('newUser');
+};
+
+const formUser = document.querySelector('#chatUser');
+const inputNickname = document.querySelector('#nickname-box');
+const nicknamesList = document.querySelector('#nickname-list');
+
+let userName = '';
+
+formUser.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const nickname = inputNickname.value;
+  localStorage.setItem('nickname', nickname);
+
+  userName = nickname;
+  socket.emit('changeName', nickname);
+});
+
+const formChat = document.querySelector('#chatForm');
+const inputMessage = document.querySelector('#message-box');
+
+formChat.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const chatMessage = inputMessage.value;
+  const nickname = userName;
+  socket.emit('message', { chatMessage, nickname });
+  inputMessage.value = '';
+  return false;
+});
+
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'message');
+  li.innerText = message;
+  messagesUl.appendChild(li);
+  return 0;
+};
+
+const addUser = (user) => {
+  const li = document.createElement('li');
+  li.setAttribute('data-testid', 'online-user');
+  li.className = user;
+  li.innerText = user;
+  nicknamesList.appendChild(li);
+  return null;
+};
+
+socket.on('restoreChat', (messageList) => {
+  if (!messageList) return 0;
+  console.log(messageList);
+  document.querySelector('#messages').innerHTML = '';
+  messageList.map(({ message, nickname, timestamp }) => createMessage(
+    `${timestamp} ${nickname}: ${message}`,
+    ));
+});
+
+socket.on('message', (message) => createMessage(message));
+
+socket.on('newUser', (userList) => {
+  nicknamesList.innerHTML = '';
+  userList.map((user) => addUser(user[1]));
+  return 0;
+});
+
+socket.on('changeName', (userList) => {
+  nicknamesList.innerHTML = '';
+  userList.map((user) => addUser(user[1]));
+});
+
+socket.on('online', (userList) => {
+  nicknamesList.innerHTML = '';
+  const userId = userList[userList.length - 1];
+  userList.pop();
+  const newList = [userId, ...userList];
+  newList.map((user) => addUser(user[1]));
+  return 0;
+});
+
+socket.on('offline', (userList) => {
+  nicknamesList.innerHTML = '';
+  userList.map((user) => addUser(user[1]));
+});
