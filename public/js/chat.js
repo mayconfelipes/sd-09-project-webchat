0f1a9c1919e7222633a589084c92a147c220cf6f
RodrigoNeoEx <draja.acc@gmail.com>
2021-08-16 18:13:58 -0300

    req02 - removed crypto, randomNick on front, front redenring chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,12 +2,36 @@ const socket = window.io();
 
 const form = document.querySelector('form');
 const chatMessage = document.querySelector('#messageInput');
-// const inputName = document.querySelector('#nickName');
+const inputName = document.querySelector('#inputNickname');
+const saveNick = document.querySelector('#btnSave');
+let nickname = '';
+
+const createRandomNickname = () => {
+  const element = document.querySelector('#userNickname');
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 1; i <= 16; i += 1) {
+    nickname += characters.charAt(Math.floor(Math.random()
+* charactersLength));
+ }
+ element.innerText = nickname;
+ return nickname;
+};
+
+createRandomNickname();
+
+saveNick.addEventListener('click', (e) => {
+  e.preventDefault();
+  nickname = inputName.value;
+  inputName.value = '';
+  return console.log(nickname);
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: chatMessage.value });
+  socket.emit('message', { chatMessage: chatMessage.value, nickname });
   chatMessage.value = '';
+  console.log(nickname.length);
   return false;
 });
 
@@ -15,9 +39,8 @@ const createMessage = (message) => {
   const messagesUl = document.querySelector('#messages');
   const li = document.createElement('li');
   li.innerText = message;
+  li.setAttribute('data-testid', 'message');
   messagesUl.appendChild(li);
 };
 
-socket.on('message', (message) => {
-  createMessage(message);
-});
+socket.on('message', (message) => createMessage(message));
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,13 +1,11 @@
 const moment = require('moment');
-const crypto = require('crypto');
 
 moment.defaultFormat = 'DD-MM-yyyy HH:mm:ss';
 
 module.exports = (io) => io.on('connection', (socket) => {
   const formatDate = moment().format();
-  const randomNick = crypto.randomBytes(8).toString('hex');
-  socket.on('message', ({ nickname = randomNick, chatMessage }) => {
-    console.log(`Mensagem ${chatMessage}`);
+  socket.on('message', ({ nickname, chatMessage }) => {
     io.emit('message', `${formatDate} - ${nickname}: ${chatMessage}`);
+    console.log(`Mensagem ${chatMessage}`);
   });
 });
