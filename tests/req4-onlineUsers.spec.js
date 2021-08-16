ec2c42b532c4f807bd53099a2d56a889e203888f
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-08-16 19:35:16 -0300

    remove log

--- tests/req4-onlineUsers.spec.js
+++ tests/req4-onlineUsers.spec.js
@@ -42,7 +42,6 @@ describe('4 - Informe a todos os clientes quem está online no momento', () => {
     //he sees his name on screen
     let client1Nicknames = await page.$$eval(dataTestid('online-user'), (nodes) => nodes.map((n) => n.innerText));
     await page.waitForTimeout(500);
-    console.log(client1Nicknames);
     expect(client1Nicknames.length).toBe(1);
     expect(client1Nicknames).toEqual(
       expect.arrayContaining([
