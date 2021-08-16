63b425909e5c7cf3b78ec6bc4b116ea4d7e07754
julianafatsil <julianafatsil@gmail.com>
2021-08-16 00:53:46 -0300

    Create start project with chatClient and Server

--- server.js
+++ server.js
@@ -1,2 +1,38 @@
-// Faça seu código aqui
-// teste
+require('dotenv').config();
+const cors = require('cors');
+const app = require('express')();
+const http = require('http').createServer(app);
+const path = require('path');
+
+const { PORT } = process.env;
+
+  const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+  });
+
+  app.use(cors());
+
+  io.on('connection', (socket) => {
+    console.log('Alguém se conectou');
+    socket.on('disconnect', () => {
+      console.log('Alguém saiu');
+    });
+    socket.on('mensagem', ({ chatMessage, nickname }) => {
+      const message = `data ${nickname} ${chatMessage}`;
+      io.emit('message', { message });
+    });
+  });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('chatClient');
+});
+
+http.listen(PORT, () => {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
+});
