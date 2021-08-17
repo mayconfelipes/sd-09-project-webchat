5587c4d2d42bf467726238a2d35f4a54ffb69408
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-08-17 12:20:38 -0300

    recomeçando logica do zero pq a correção do pull comeu um arquivo

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
 // Faça seu código aqui
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  console.log('bichao entro');
+
+  socket.on('disconnect', () => {
+    console.log('bichao saiu  - deisconnect');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`vrawwwwwww na porta : ${PORT}`);
+});
