0aa24e5b53957703af884e9907fcb17d083d272f
Christopher <chris.tryber@gmail.com>
2021-08-17 13:48:55 -0300

    requisito 1 e 2 passando

--- server.js
+++ server.js
@@ -18,23 +18,37 @@ const server = app.listen(PORT, () => console.log(`Server rodando na porta ${POR
 
 const io = socketIO(server);
 
-const onlineUsers = [];
+let onlineUsers = [];
 
-io.on('connection', (socket) => {
-    console.log('New connection', socket);
-    // const newUser = socket.id.slice(4);
-    // onlineUsers.push(newUser);
-    // console.log(onlineUsers);
-
-    socket.emit('newConnection', onlineUsers);
-    
-    socket.on('message', ({ chatMessage, nickname }) => {
+const socketOn = {
+    message: ({ chatMessage, nickname }) => {
+        console.log(nickname);
         const date = getFullDate();
         const messageFormated = formatMessage(date, nickname, chatMessage);
         io.emit('message', messageFormated);
-    });
+    },
 
-    socket.on('updateNick', (newNick) => {
-        socket.emit('updateNick', newNick);
-    });
+    updateNick: (socket) => (({ newNick, id }) => {
+        const response = onlineUsers.map((user) => {
+            if (socket.id.includes(user.id)) return { name: newNick, id };
+            return user;
+        });
+        onlineUsers = response;
+        io.emit('updateOnlineUsers', response);
+    }),
+};
+
+io.on('connection', (socket) => {
+    const newUser = {
+        id: socket.id.slice(0, 16),
+        name: '',
+    };
+    
+    onlineUsers.push(newUser);
+    
+    io.emit('updateOnlineUsers', onlineUsers);
+    
+    socket.on('message', socketOn.message);
+
+    socket.on('updateNick', socketOn.updateNick(socket));
 });
--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,7 @@
 const getFullDate = require('./getFullDate');
 const formatMessage = require('./formatMessage');
-const genereteNick = require('./genereteNick');
 
 module.exports = {
     getFullDate,
     formatMessage,
-    genereteNick,
 };
