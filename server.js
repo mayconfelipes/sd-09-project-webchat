d0ca3ffcefb627282a4701260fcc74493b4f65af
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-08-17 17:39:27 -0300

    refazendo do 0 etapa 1

--- server.js
+++ server.js
@@ -2,25 +2,61 @@
 
 const express = require('express');
 const http = require('http');
+const { slice } = require('lodash');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const CreateHist = require('./models/messageModel');
 
 const PORT = 3000;
 
+let chaters = {};
+// let name = '';
+
 app.get('/', (req, res) => {
-  res.send(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/index.html`);
 });
+
+
 
 io.on('connection', (socket) => {
-  console.log('bichao entro');
 
+  // novo chater
+  chaters[socket.id] = {
+    chatMessage: '',
+    name: socket.id.slice(0, 16),
+  };
+  
+  //aviso connect
+  console.log(`${chaters[socket.id].name} entro bichao`);
+
+  
+  
+  // aviso desconect
   socket.on('disconnect', () => {
-    console.log('bichao saiu  - deisconnect');
+    console.log(`${chaters[socket.id].name} entro bichao`);
   });
 });
+
+
+// envio de mensagem
+io.on('connection', (socket) => {  
+  socket.on('message', (message) => {
+    const { chatMessage } = message;
+    let { nickname } = message;
+
+    if (nickname === '') { nickname = chaters[socket.id].name; }
+    const msg = `${new Date().toLocaleString('es-CL')} - ${nickname}: ${chatMessage}`
+      io.emit('message', msg);
+    console.log(`bichao o ${nickname} mandou a braba: ${chatMessage}`)
+
+    
+    // CreateHist.createMessage(chatMessage, nickname);
+  })
+
+})
 
 server.listen(PORT, () => {
   console.log(`vrawwwwwww na porta : ${PORT}`);
