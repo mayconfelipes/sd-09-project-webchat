bc4f3e6ecff04515055f3dac6a55421b2ba553fc
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-08-17 08:15:01 -0300

    [feat][requirement 2]

--- public/js/webChat.js
+++ public/js/webChat.js
@@ -1,33 +1,60 @@
 const socket = window.io();
 // função io é uma função injetada ao objeto window do DOM da página
 
+let userNick = '';
 const messageBtn = document.querySelector('#messageBtn');
-const userNickname = document.querySelector('#nickname');
-const messageInput = document.querySelector('#message');
+const messageInput = document.querySelector('#messageInput');
 const msgContainer = document.querySelector('#msgContainer');
+const nickBtn = document.querySelector('#nickBtn');
+const nickInput = document.querySelector('#nickInput');
+const usersContainer = document.querySelector('#usersContainer');
+const DATA_TEST_ID = 'data-testid';
+
+const updateNick = (nick) => {
+  userNick = nick;
+  const user = document.querySelector('#user');
+  const userId = document.querySelector('#userId');
+  if (userId) {
+    user.removeChild(userId);
+  }
+  const nickname = document.createElement('span');
+  nickname.setAttribute(DATA_TEST_ID, 'online-user');
+  nickname.setAttribute('id', 'userId');
+  nickname.innerText = nick;
+  user.appendChild(nickname);
+};
+
+nickBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  socket.emit('updateNick', { defaultNick: userNick, newNick: nickInput.value });
+  updateNick(nickInput.value);
+  nickInput.value = '';
+  return false;
+});
 
 messageBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  socket.emit('message', { chatMessage: messageInput.value, nickname: userNickname });
+  socket.emit('message', { chatMessage: messageInput.value, nickname: userNick });
   messageInput.value = '';
   return false;
 });
 
 const addMessage = (message) => {
   const msg = document.createElement('p');
-  msg.setAttribute('data-testid', 'message');
+  msg.setAttribute(DATA_TEST_ID, 'message');
   msg.innerText = message;
   msgContainer.appendChild(msg);
 };
 
-const createResponse = (response) => {
-  const user = document.querySelector('#user');
-  const nick = document.createElement('span');
-  nick.setAttribute('id', 'nickname');
-  nick.setAttribute('data-testid', 'online-user');
-  nick.innerText = response;
-  user.appendChild(nick);
+const listUsers = (users) => {
+  users.forEach((user) => {
+    const liUser = document.createElement('li');
+    liUser.setAttribute(DATA_TEST_ID, 'online-user');
+    liUser.innerText = user.nickname;
+    usersContainer.appendChild(liUser);
+  });
 };
 
-socket.on('connected', (res) => createResponse(res));
+socket.on('connected', (nick) => updateNick(nick));
+socket.on('onlineUsers', (users) => listUsers(users));
 socket.on('message', (message) => addMessage(message));
--- socket/webChatSocket.js
+++ socket/webChatSocket.js
@@ -17,8 +17,14 @@ function ioWebChat(io) {
     io.emit('connected', nick);
     io.emit('onlineUsers', onlineUsers);
 
-    socket.on('message', ({ chatMessage, nickname }) => {
+    socket.on('updateNick', ({ defaultNick, newNick }) => {
       // aqui a funcao recebe a informacao do emit e chama o model
+      const user = onlineUsers.findIndex((u) => u.nickname === defaultNick);
+      onlineUsers[user].nickname = newNick;
+      io.emit('onlineUsers', onlineUsers);
+    });
+
+    socket.on('message', ({ chatMessage, nickname }) => {
       const timeStamp = moment().format('DD-MM-yyyy HH:mm:ss');
       io.emit('message', `${timeStamp} - ${nickname}: ${chatMessage}`);
       WebChatModel.addNewMessage({ message: chatMessage, nickname, timeStamp });
