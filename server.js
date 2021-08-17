933a5bbb65f47e12fd45f0c35094a990039cf754
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-08-16 21:48:17 -0300

    initial commit

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`);
+});
+
+app.use('/', (_req, res) => res.render('index'));
+
+server.listen(3000, () => console.log('conectado na porta 3000'));
