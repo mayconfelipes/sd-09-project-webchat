65846edf5d5d296dc863e44f63913502d87ba6f8
brenno-calado <brenno.calado@gmail.com>
2021-08-17 12:10:49 -0300

    initial commit using ES6

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-// Faça seu código aqui
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
+
+const dirname = path.resolve();
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(socket);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(dirname, '/views/index.html'));
+});
+
+httpServer.listen(3000, () => { console.log('ouvindo na porta 3000'); });
