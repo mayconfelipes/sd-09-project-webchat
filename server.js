07dc9f9b51fbf4d43998b511b0f2747f6ba0ed40
Matheus Martino <matheus.martino97@gmail.com>
2021-08-17 18:16:42 -0300

    Requisito 3.

--- server.js
+++ server.js
@@ -5,20 +5,24 @@ const { PORT = 3000 } = process.env;
 const path = require('path');
 const cors = require('cors');
 const moment = require('moment');
-const { indexOf } = require('lodash');
+const socketIO = require('socket.io');
 
 const bodyParser = require('body-parser').json();
 const app = require('express')();
 const server = require('http').createServer(app);
 
+const connection = require('./models/connection');
+const ChatController = require('./controllers/Chat');
+const ChatModel = require('./models/Chat');
+
 const sockets = [];
 const serializeId = (id) => {
   const trimId = id.slice(0, 16);
-  
+
   return trimId;
 };
 
-const io = require('socket.io')(server, {
+const io = socketIO(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
@@ -37,6 +41,20 @@ app.use(
   }),
 );
 
+app.get('/', async (_req, res) => {
+  const messages = await ChatController.findAll(connection, ChatModel);
+
+  return res.render('chat', { sockets, messages });
+});
+
+app.get('/chat', async (_req, res) => {
+  const messages = await ChatController.findAll(connection, ChatModel);
+
+  return res.status(200).json(messages);
+});
+
+app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
+
 io.on('connection', (socket) => {
   const trimId = serializeId(socket.id);
   const socketWithTrimId = { ...socket, trimId };
@@ -44,11 +62,17 @@ io.on('connection', (socket) => {
   sockets.push(socketWithTrimId);
 
   socket.on('disconnect', () => {
-    sockets.splice(indexOf(socketWithTrimId), 1);
+    sockets.splice(sockets.indexOf(socketWithTrimId), 1);
   });
 
   socket.on('message', ({ chatMessage, nickname }) => {
-    io.emit('message', `${moment().format('DD-MM-yyyy LTS')} - ${nickname}: ${chatMessage}`);
+    const timestamp = moment().format('DD-MM-yyyy LTS');
+
+    ChatController.create(
+      connection, ChatModel, { chatMessage, nickname, timestamp },
+    );
+
+    io.emit('message', `${timestamp} - ${nickname}: ${chatMessage}`);
   });
 
   socket.on('nicknameUpdate', ({ target, newNickname }) => {
@@ -58,10 +82,6 @@ io.on('connection', (socket) => {
   socket.emit('welcome', trimId);
   io.emit('login', trimId);
 });
-
-app.get('/', (_req, res) => res.render('chat', { sockets }));
-
-app.get('/ping', (_req, res) => res.status(200).json({ message: 'pong' }));
 
 server.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
