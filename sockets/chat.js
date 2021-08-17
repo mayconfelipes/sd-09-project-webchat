4380d7a332415029def149966499e5732e9b5487
marcusWittho <mcswitthoeft@gmail.com>
2021-08-17 09:26:15 -0300

    Requisito 1

--- sockets/chat.js
+++ sockets/chat.js
@@ -2,13 +2,13 @@ module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', ({ chatMessage, nickname }) => {
     const fullDate = new Date();
     const day = fullDate.getDate();
-    const month = (fullDate.getMonth() + 1);
+    const month = fullDate.getMonth();
     const year = fullDate.getFullYear();
 
     const hours = fullDate.getHours();
     const minutes = fullDate.getMinutes();
 
-    const usrMsg = `${day}/${month}/${year} ${hours}:${minutes} ${nickname}: ${chatMessage}`;
+    const usrMsg = `${day}-${month}-${year} ${hours}:${minutes} ${nickname}: ${chatMessage}`;
     console.log(`${usrMsg}`);
     io.emit('message', usrMsg);
   });
