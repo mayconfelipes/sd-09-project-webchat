255e4b81903b5dbb5d6774fa6545059a9b05effd
ViniciusChagas <viniciushrcs@gmail.com>
2021-08-18 09:20:32 -0300

    Consertando Webchat controller

--- controller/webchat.js
+++ controller/webchat.js
@@ -1,14 +1,10 @@
-const { Router } = require('express');
-
 const webchatModel = require('../models/webchat');
 
-const router = Router();
+const getAllMessages = async (_req, res) => {
+  const messages = await webchatModel.getAllMessages();
+  const history = messages.map(({ message, nickname, timestamp }) =>
+    `${timestamp} - ${nickname}: ${message}`);
+  res.status(200).render('index.ejs', { history });
+};
 
-router.get('/', async (_req, res) => {
-    const messages = await webchatModel.getAllMessages();
-    const history = messages.map(({ message, nickname, timestamp }) =>
-      `${timestamp} - ${nickname}: ${message}`);
-    res.status(200).render('index.ejs', { history });
-  });
-
-module.export = router;
+module.export = getAllMessages;
---
+++ socket/webchat.js
@@ -0,0 +1,31 @@
+const moment = require('moment');
+const webchatModel = require('../models/webchat');
+
+const onlineUsers = [];
+const getTimestamp = moment().format('DD-MM-yyyy HH:mm:ss');
+
+function ioWebchat(io) {
+ io.on('connection', async (socket) => {
+    const userId = socket.id.slice(0, 16);
+    onlineUsers.push({ userId, nickname: userId });
+    io.emit('connected', { nick: userId, users: onlineUsers });
+
+    socket.on('updateNick', ({ newNick }) => {
+        const user = onlineUsers.findIndex((u) => u.nickname === userId);
+        onlineUsers[user].nickname = newNick;
+        io.emit('refreshUsers', { users: onlineUsers });
+    });
+
+    socket.on('message', async ({ chatMessage, nickname }) => {
+        io.emit('message', `${getTimestamp} - ${nickname}: ${chatMessage}`);
+        await webchatModel.addNewMessage({ message: chatMessage, nickname, timestamp: getTimestamp });
+      });
+
+      socket.on('disconnect', () => {
+        const userIndex = onlineUsers.findIndex((u) => u.userId === userId);
+        onlineUsers.splice(userIndex, 1); io.emit('disconnectUser', { users: onlineUsers });
+        });
+    });   
+}
+
+module.exports = ioWebchat;
