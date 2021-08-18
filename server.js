59c17b2f2566221d7f8e88ee97f3bae8112111f5
Leticia Lima <letylima96@gmail.com>
2021-08-18 20:03:48 -0300

    requisito 1

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-// Faça seu código aqui
+const express = require('express');
+const { format } = require('date-fns');
+const path = require('path');
+
+const date = format(new Date(), 'dd-MM-yyyy hh:mm:ss');
+
+const app = express();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/view/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} is connected`);
+  socket.on('message', ({ chatMessage, nickname }) => {
+    io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
+  });
+});
+
+app.use(express.static('public'));
+
+server.listen(3000, () => console.log('Escutando porta 3000'));
