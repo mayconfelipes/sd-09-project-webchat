f1ead6fd0fdb086f92b0d72e7d2def9eb3c077f1
ViniciusChagas <viniciushrcs@gmail.com>
2021-08-18 08:53:53 -0300

    Servidor montado

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
