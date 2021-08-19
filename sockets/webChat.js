174cc4d54eef7ab735fb7ced78862613ad0c1a97
Thiago Granville <thiagogranville@gmail.com>
2021-08-18 21:06:56 -0300

    [FEAT][Req 02][Front End]

--- sockets/webChat.js
+++ sockets/webChat.js
@@ -4,15 +4,19 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     const currentTime = moment().format('DD-MM-yyyy LTS');
 
-    // socket.emit('message', `Bem vindo user${socket.id}`);
-    // socket.broadcast.emit('message', `user${socket.id} entrou no chat`)
+    socket.emit('serverMessage', 'Bem vindo ao TrybeChat');
+    socket.broadcast.emit('serverMessage', `user${socket.id} entrou no chat`);
 
-    // socket.on('disconnect', () => {
-    //   socket.broadcast.emit('message', `user${socket.id} saiu do chat`);
-    // });
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('serverMessage', `user${socket.id} saiu do chat`);
+    });
 
     socket.on('message', ({ chatMessage, nickname }) => {
-      io.emit('message', `${currentTime} - ${nickname || `user${socket.id}`}: ${chatMessage}`);
+      io.emit('message', `${currentTime} - ${nickname}: ${chatMessage}`);
+    });
+
+    socket.on('changeNickname', ({ nickname, newNickname }) => {
+      io.emit('serverMessage', `${nickname} agora é ${newNickname}`);
     });
   });
 };
