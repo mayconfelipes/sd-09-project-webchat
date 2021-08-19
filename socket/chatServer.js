453c1370414b0c7f4bf3cbfd5f62f6bd24bb27df
julianafatsil <julianafatsil@gmail.com>
2021-08-18 21:03:08 -0300

    Creation of listAllUsers event on Front and Back Socket

--- socket/chatServer.js
+++ socket/chatServer.js
@@ -2,19 +2,16 @@ const socketIo = require('socket.io');
 const { getDataHora } = require('../public/helpers/usersFunctions');
 
 const arrayMessages = [];
+const arrayUsers = [];
 
 module.exports = (http) => {
   const io = socketIo(http, {
-    cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    },
-    });
-
+    cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST', 'PUT', 'DELETE'] } });
     io.on('connection', (socket) => {
       console.log('Alguém se conectou');
       // listar histórico
       socket.emit('listAllMessages', arrayMessages);
+      socket.emit('listAllUsers', arrayUsers);
       socket.on('disconnect', () => {
         console.log('Alguém saiu');
       });
