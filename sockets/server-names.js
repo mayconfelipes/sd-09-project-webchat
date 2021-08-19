daec5bf7026746a198ca501be840078b11e785d6
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-08-19 16:07:29 -0300

    requisito 4 - mudanças estruturais

--- sockets/server-names.js
+++ sockets/server-names.js
@@ -7,19 +7,17 @@ const onConnect = (io) => {
 
     socket.on('sendName', (data) => {
       nickNames.push(data);
-      socket.broadcast.emit('receivedNames', nickNames.map(nick));
-      socket.emit('receivedNames', nickNames.map(nick));
+      socket.broadcast.emit('receivedNames', nickNames);
+      socket.emit('receivedNames', nickNames);
     });
 
     socket.on('changeName', (objNames) => {
       const { oldName, newName } = objNames;
       const names = nickNames.map(nick);
       const index = names.indexOf(oldName);
-      const newObj = nickNames[index];
-      console.log(newObj);
-      newObj.nickname = newName;
-      socket.broadcast.emit('receivedNames', nickNames.map(nick));
-      socket.emit('receivedNames', nickNames.map(nick));
+      nickNames[index].nickname = newName;
+      socket.broadcast.emit('receivedNames', nickNames);
+      socket.emit('receivedNames', nickNames);
     });
   });
 };
@@ -28,15 +26,13 @@ const onDisconnect = (io) => {
   io.on('connection', async (socket) => {
     socket.on('disconnect', () => {
       nickNames = nickNames.filter((item) => item.id !== socket.id);
-      socket.broadcast.emit('receivedNames', nickNames.map(nick));
-      socket.emit('receivedNames', nickNames.map(nick));
+      socket.broadcast.emit('receivedNames', nickNames);
+      socket.emit('receivedNames', nickNames);
     });
   });
 };
 
-const together = (io) => {
+module.exports = (io) => {
   onDisconnect(io);
   onConnect(io);
 };
-
-module.exports = together;
