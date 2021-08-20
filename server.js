783e4c2fb3d351a0b73739f51371a452ca9990d6
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-08-19 22:36:33 -0300

    req04 - trollo mais valeu [novos jutsus ]

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { instrument } = require('@socket.io/admin-ui');
 const express = require('express');
 require('dotenv').config();
 
@@ -8,7 +9,7 @@ const PORT = 3000;
 
 const io = require('socket.io')(http, {
     cors: {
-        origin: 'https://localhost:3000',
+        origin: ['http://localhost:3000', 'http://admin.socket.io/'],
         methods: ['GET', 'POST'],
     },
 });
@@ -20,6 +21,8 @@ require('./sockets/webChat')(io);
 app.get('/', (_req, res) => {
     res.sendFile(`${__dirname}/web/index.html`);
 });
+
+instrument(io, { auth: false });
 
 http.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
--- sockets/webChat.js
+++ sockets/webChat.js
@@ -9,15 +9,14 @@ module.exports = (io) => io.on('connection', async (socket) => {
     // https://tsh.io/blog/socket-io-tutorial-real-time-communication/
     const { id } = socket;
     const random = id.slice(0, -4);
-  
-    const messageList = await Messages.getAll();
-    io.emit('restoreChat', messageList);
     
     socket.on('newUser', () => {
       userList.set(random, random);
       socket.broadcast.emit('newUser', [...userList]);
       socket.emit('online', [...userList]);
     });
+    const messageList = await Messages.getAll();
+    io.emit('restoreChat', messageList); 
   
     socket.on('changeName', (nickname) => {
       userList.set(random, nickname);
